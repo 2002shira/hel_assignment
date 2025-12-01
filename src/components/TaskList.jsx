bc6e459c39f13d7b50f81c73b4cfc8bcc22fe5b7
2002shira <shiraprober@gmail.com>
2025-12-01 23:56:35 +0200

    added carousel

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -5,13 +5,17 @@ import "../styles/TaskList.css";
 const TaskList = ({ tasks = [], onEdit, onDelete }) => {
   if (!tasks || tasks.length === 0) return <p className="no-tasks">No tasks yet!</p>;
 
+  const displayTasks = [...tasks, ...tasks];
+
   return (
-    <div className="task-list">
-      {tasks.map((task) => (
-        <div className="task-list-item" key={task.id}>
-          <TaskItem task={task} onEdit={onEdit} onDelete={onDelete} />
-        </div>
-      ))}
+    <div className="task-list-container vertical-carousel">
+      <div className="task-list carousel">
+        {displayTasks.map((task, index) => (
+          <div className="task-list-item" key={`${task.id}-${index}`}>
+            <TaskItem task={task} onEdit={onEdit} onDelete={onDelete} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
